ce3fbca7a1e0ad303ebf7b5ebf1a1659c0cc0f6c
Jatin Jain <jatin321jain@gmail.com>
2025-05-20 04:35:19 +0530

    reversed linked list using iterative method

--- java/Main.java
+++ java/Main.java
@@ -1,26 +1,72 @@
 class Main{
-    public static void main(String[] args){
-        Node temp1=head1;
-        Node temp2=head2;
-        Node head=new Node(100);
-        Node temp=head;
-        while(temp1!=null&&temp2!=null){
-            if(temp1.val<temp2.val){
-                temp.next=temp1;
-                temp=temp.next;
-                temp1=temp1.next;
+    static class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val=val;
+        }
+    }
+    static void removeDuplicates(Node head){
+        Node previous=head;
+        Node current=head.next;
+        while(current!=null){
+            if(current.val==previous.val){
+                previous.next=previous.next.next;
+                current=current.next;
             }else{
-                temp.next=temp2;
-                temp=temp.next;
-                temp2=temp2.next;
+                previous=previous.next;
+                current=current.next;
             }
         }
-        if(temp1!=null){
-            temp.next=temp1;
-        }else{
-            temp.next=temp2;
-        }
-        head=head.next;
-        return head;
     }
+    static void display(Node head){
+        while(head!=null){
+            System.out.print(head.val+" ");
+            head=head.next;
+        }
+        System.out.println();
+    }
+    static Node reverse(Node head){
+        Node previous = null;
+        Node current=head;
+        Node next_ = head.next;
+        while(true){
+            current.next=previous;
+            if(next_==null){head=current;return head;}
+            else{
+            previous=current;
+            current=next_;
+            next_=next_.next;}
+        }
+    }
+    public static void main(String[] args){
+    Node a=new Node(10);
+    Node b=new Node(2);
+    Node c=new Node(3);
+    Node d=new Node(4);
+    Node e=new Node(5);
+    Node f=new Node(6);
+    Node g=new Node(7);
+    Node h=new Node(8);
+    Node i=new Node(9);
+    Node j=new Node(10);
+    Node k=new Node(11);
+    a.next=b;
+    b.next=c;
+    c.next=d;
+    d.next=e;
+    e.next=f;
+    f.next=g;
+    g.next=h;
+    h.next=i;
+    i.next=j;
+    j.next=k;
+    k.next=null;
+    display(a);
+    // removeDuplicates(a);
+    // display(a);
+    a=reverse(a);
+    display(a);
+    
+}
 }
